f77b2e6974e1026e4c7afd7dac3da2ccd52e7f7a
食盐Emon <2945921033@qq.com>
2020-04-25 11:07:41 +0800

    修改为官方机型代号

--- omni_xihu.mk
+++ omni_xihu.mk
@@ -5,7 +5,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := xihu
+PRODUCT_DEVICE := msm8909w
 PRODUCT_NAME := omni_xihu
 PRODUCT_BRAND := huami
 PRODUCT_MODEL := Amazfit Smartwatch2
